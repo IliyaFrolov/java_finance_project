125272a8d3ed5da2898f5cbde4405b5a12f78297
Maddex22 <iliya22@outlook.com>
2021-12-27 20:18:30 +0000

    Added categories and savings to summary. Added user input.

--- src/Account.java
+++ src/Account.java
@@ -1,33 +1,51 @@
+import java.util.Map;
+
 class Account {
     private double balanceBefore;
     private double interest;
     private double income;
     private double expenses;
     private double balanceAfter = 0;
+    private double savings;
+    private Map<String, Double> categories;
 
-    Account(double balanceBefore, double interest, double income, double expenses) {
+    Account(double balanceBefore, double interest, double income, double expenses, Map<String, Double> categories) {
         this.balanceBefore = balanceBefore;
         this.interest = interest;
         this.income = income;
         this.expenses = expenses;
+        this.categories = categories;
     }
 
     public void printReport() {
-        final String SUMMARY = "Summary for this month's finances:\n" 
+        calcFinances();
+        String summary = "Summary for this month's finances:\n" 
         + "Initial Balance - %.2f\n"
         + "Income - %.2f\n"
         + "Expenses - %.2f\n"
         + "Interest Earned - %.2f\n"
+        + "Savings - %.2f\n"
         + "Total Balance - %.2f";
-        calcBalance();
+
         System.out.println(String.format(
-            SUMMARY, balanceBefore, income, 
+            summary, balanceBefore, income, 
             expenses, balanceBefore*interest, 
-            balanceAfter
+            savings, balanceAfter
         ));
+        printCatBreakdown();
     }
 
-    private void calcBalance() {
+    private void calcFinances() {
         balanceAfter = balanceBefore + income - expenses + balanceBefore*interest;
+        double temp = balanceAfter - balanceBefore;
+        savings = temp < 0 ? 0 : temp;
+    }
+
+    private void printCatBreakdown() {
+        System.out.println("\nCategory Breakdown:");
+
+        for (var category : categories.entrySet()) {
+            System.out.println(String.format("%s - %.2f", category.getKey(), category.getValue()));
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
+import java.util.Map;
+import java.util.Scanner;
+
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        Account myAcc = new Account(1245.64, 0.25, 523.54, 142.78);
+        Map<String, Double> categories = new HashMap<>();
+        categories.put("Entertainment", 30.00);
+        categories.put("Memberships", 70.00);
+        categories.put("Eating out", 90.00);
+        categories.put("Groceries", 100.00);
+        categories.put("Other", 5.00);
+        /*
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Enter your current balance:");
+        Double balanceBefore = reader.nextDouble();
+
+        System.out.println("Enter your income:");
+        Double income = reader.nextDouble();
+
+        System.out.println("Eating out:");
+        categories.put("Eating out", reader.nextDouble());
+
+        reader.close();
+        */
+        Account myAcc = new Account(1000, 0.25, 500, 700, categories);
         myAcc.printReport();
     }
 }
